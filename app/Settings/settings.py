b975fe501e632fe5d2a7c8d23ffe30590e52c8d9
Jorge Costa <jorgemdcosta@gmail.com>
2023-09-25 20:27:39 +0100

    Add rabbitmq settings and client

--- app/Settings/settings.py
+++ app/Settings/settings.py
@@ -11,7 +11,10 @@ NODE_ID: str = config('NODE_ID')
 NODE_DOMAIN: str = config('NODE_DOMAIN')
 FASTAPI_HOST: str = config('FASTAPI_HOST', default="127.0.0.1")
 FASTAPI_PORT: int = config('FASTAPI_PORT', default=8000)
-
+RABBITMQ_USER=config('RABBITMQ_USER')
+RABBITMQ_PASSWORD=config('RABBITMQ_PASSWORD')
+RABBITMQ_HOST=config('RABBITMQ_HOST')
+RABBITMQ_PORT=config('RABBITMQ_PORT')
 
 
 ROOT_DIR = Path(__file__).parent.parent.parent
@@ -31,9 +34,23 @@ class BaseConfig(BaseSettings):
     node_domain: str = NODE_DOMAIN
     fastapi_host: str = FASTAPI_HOST
     fast_api_port: str = FASTAPI_PORT
+    rabbitmq_user: str = RABBITMQ_USER
+    rabbitmq_password: str = RABBITMQ_PASSWORD
+    rabbitmq_host: str = RABBITMQ_HOST
+    rabbitmq_port: str = RABBITMQ_PORT
     s3_bucket: str = ""
 
 class Settings(BaseConfig):
     pass
 
 settings = Settings()
+
+class RabbitMQClientSettings():
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    ssl_context.set_ciphers('ECDHE+AESGCM:!ECDSA')
+    url = f"amqps://{settings.rabbitmq_user}:{settings.rabbitmq_password}@{settings.rabbitmq_host}:{settings.rabbitmq_port}"
+
+    class Confg:
+        env_file_encoding= "utf-8"
+
+rabbitmq_settings = RabbitMQClientSettings()
---
+++ app/clients/rabbitMQClient.py
@@ -0,0 +1,108 @@
+from functools import lru_cache
+import json
+import logging
+import threading
+import time
+import uuid
+from anyio import sleep
+import pika
+from typing import Callable
+from app.configuration.settings import Environments, settings, rabbitmq_settings
+from app.domains.train_model.schemas.constants import TrainJobInstanceStatus
+
+from app.domains.train_model.schemas.train_queue import TrainJobQueue
+
+logger = logging.getLogger(__name__) 
+
+
+class RabbitMQClient:
+    def __init__(self, connection: pika.BlockingConnection):
+        self.connection = connection
+        self.train_job_channel = self.connection.channel()
+        self.consume_train_jobs_channel = self.connection.channel()
+        self.train_job_status_update_channel = self.connection.channel()  # Create a separate channel for status updates
+        self.is_consuming = True
+
+        self.train_job_channel.basic_qos(prefetch_count=1)
+        self.consume_train_jobs_channel.basic_qos(prefetch_count=1)
+        self.train_job_status_update_channel.basic_qos(prefetch_count=1)  # Set QoS for the status update channel
+        
+        self.train_job_lock = threading.Lock()
+        self.train_job_status_uptade_lock = threading.Lock()
+        self.consume_jobs_lock = threading.Lock()
+        
+        try:
+            self.train_job_channel.queue_declare(queue='jobs', durable=True, arguments={'x-max-priority': 5})
+            self.train_job_status_update_channel.queue_declare(queue='train_job_status_update', durable=True)  # Use the separate channel to declare the queue
+        except pika.exceptions.ChannelClosedByBroker as e:
+            logging.error(f"Failed to declare queue: {e}")
+            raise
+
+    def enqueue_job(self, job_data: 'TrainJobQueue', priority: int):
+        job_json = job_data.model_dump_json()
+        with self.train_job_lock:
+            self.train_job_channel.basic_publish(
+                exchange='',
+                routing_key='jobs',
+                body=job_json,
+                properties=pika.BasicProperties(
+                    delivery_mode=2,
+                    priority=priority
+                )
+            )
+
+    def enqueue_train_job_status_update(self, run_id: uuid.UUID, status: 'TrainJobInstanceStatus'):
+        status_update = {
+            'run_id': str(run_id),
+            'status': status.value
+        }
+
+        with self.train_job_status_uptade_lock:
+            self.train_job_status_update_channel.basic_publish(  # Use the separate channel to publish the message
+                exchange='',
+                routing_key='train_job_status_update',
+                body=json.dumps(status_update),
+                properties=pika.BasicProperties(
+                    delivery_mode=2,
+                )
+            )
+
+    def consume_jobs(self, callback: Callable):
+        thread = threading.Thread(target=self._process_data_events, args=(callback, 'jobs'))
+        thread.setDaemon(True)
+        thread.start()
+
+    def _process_data_events(self, callback, queue_name):
+        self.consume_train_jobs_channel.basic_consume(queue=queue_name, on_message_callback=callback)
+        logging.info(f"Start consuming {queue_name}")
+        while self.is_consuming:
+            with self.train_job_lock:
+                self.consume_train_jobs_channel.connection.process_data_events()
+            time.sleep(0.1)  # Add sleep to reduce CPU usage
+
+        logging.info(f"Stop consuming {queue_name}")
+
+    def acknowledge_job(self, delivery_tag):
+        logger.info(f"tag: {delivery_tag}")
+        with self.train_job_lock:
+            self.consume_train_jobs_channel.basic_ack(delivery_tag=delivery_tag)
+        logger.info(f"confirmed tag: {delivery_tag}")
+
+    def close(self):
+        with self.train_job_lock:
+            self.is_consuming = False
+
+@lru_cache
+def get_rabbitmq_client() -> RabbitMQClient:
+    
+    if settings.env == Environments.LOCAL:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
+        return RabbitMQClient(connection)
+
+    else:
+
+        parameters = pika.URLParameters(rabbitmq_settings.url)
+        parameters.ssl_options = pika.SSLOptions(context=rabbitmq_settings.ssl_context)
+
+        connection = pika.BlockingConnection(parameters)
+        return RabbitMQClient(connection) 
