a0486d568a72df6a66376fc89dcd15b72567fd75
Jorge Costa <jorgemdcosta@gmail.com>
2023-09-27 21:34:31 +0100

    Fix router import

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 import logging
 
+from app.domains.train_model.controllers.train_model import on_train_model_router_startup
+
 logging.config.fileConfig('logging.conf', disable_existing_loggers=False)  #type: ignore
 
 app = FastAPI()
 
 
-app.include_router(train_model_router)
+app.include_router(on_train_model_router_startup)
